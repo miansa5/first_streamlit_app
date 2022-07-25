d9eed1bdd0ea369c8f0e6d5d24e2be1bcea330b5
Mian <109996400+miansa5@users.noreply.github.com>
2022-07-26 04:15:25 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach and Rocket Smoothie')
 streamlit.text('Hard-boiled Free Range Egg')
+streamlit.text('Avocado Toast')
